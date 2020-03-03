09649776a1027ba71caa61eb34d26b9deffe8fdd
powerbuoy <powerbuoy@gmail.com>
2020-03-03 01:46:24 +0100

    Cleanup

--- src/js/sleek-ui.js
+++ src/js/sleek-ui.js
@@ -4,7 +4,6 @@ export { default as CountTo } from './count-to.js';
 export { default as Dialog } from './dialog.js';
 export { default as DocumentOutline } from './document-outline.js';
 export { default as FilterItems } from './filter-items.js';
-export { default as GoogleMap } from './google-map.js';
 export { default as ScrollStats } from './scroll-stats.js';
 export { default as Scrollspy } from './scrollspy.js';
 export { default as Slideshow } from './slideshow.js';
