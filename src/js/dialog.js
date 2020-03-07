b75d781e809ee4b34e216e83d8d890cc5a0e836b
powerbuoy <powerbuoy@gmail.com>
2020-03-07 23:35:19 +0100

    Refactored Dialog

--- src/js/dialog.js
+++ src/js/dialog.js
@@ -1,103 +1,96 @@
 'use strict';
 
-export default class Dialog {
-	constructor (conf) {
+export class DialogTrigger {
+	constructor (el, conf) {
+		this.el = el;
 		this.config = Object.assign({
-			triggers: document.querySelectorAll('a[href^="#dialog-"]')
+			target: document.getElementById(this.el.getAttribute('href').substr(1)),
+			templateDialog: null
 		}, conf);
 	}
 
 	mount () {
-		this.mountTemplateDialog();
-		this.mountDialogs();
-		this.mountTriggers();
-	}
+		this.el.addEventListener('click', e => {
+			e.preventDefault();
 
-	mountTemplateDialog () {
-		this.templateDialog = document.createElement('div');
+			// The target is a template
+			if (this.config.target.nodeName.toLowerCase() === 'script') {
+				if (this.config.templateDialog) {
+					this.config.templateDialog.className = 'dialog dialog--no-transition ' + this.config.target.className;
+					this.config.templateDialog.innerHTML = this.config.target.innerHTML + '<a class="dialog__close">&times;</a>';
 
-		this.templateDialog.classList.add('dialog');
-		document.body.appendChild(this.templateDialog);
-	}
-
-	mountDialogs () {
-		document.querySelectorAll('div.dialog').forEach(dialog => {
-			// Insert methods
-			dialog.sleekDialog = {
-				open: () => {
-					document.documentElement.classList.add('dialog-open');
-					document.documentElement.classList.add('dialog-open--' + dialog.id);
-					dialog.classList.add('open');
-				},
-				close: () => {
-					document.documentElement.classList.remove('dialog-open');
-					document.documentElement.classList.remove('dialog-open--' + dialog.id);
-					dialog.classList.remove('open');
-				},
-				isOpen: () => {
-					return dialog.classList.contains('open');
-				},
-				isClosed: () => {
-					return !dialog.classList.contains('open');
-				},
-				getStatus: () => {
-					return dialog.classList.contains('open') ? 'open' : 'close';
+					// HACK: Wait for dialog--no-transition to kick in (for some reason I need around 50ms...)
+					setTimeout(() => {
+						this.config.templateDialog.classList.remove('dialog--no-transition');
+						this.config.templateDialog.sleekDialog.open();
+					}, 50);
 				}
-			};
-
-			// Insert backdrop
-			var backdrop = document.createElement('div');
-
-			backdrop.classList.add('backdrop');
-			dialog.parentNode.insertBefore(backdrop, dialog.nextSibling);
-
-			backdrop.addEventListener('click', e => {
-				if (e.target === backdrop) {
-					dialog.sleekDialog.close();
+				else {
+					console.error('DialogTrigger points to a script but no template dialog is specified');
 				}
-			});
-
-			// Insert close button
-			var close = document.createElement('a');
-
-			close.classList.add('dialog__close');
-			close.innerHTML = '&times;';
-			dialog.appendChild(close);
-
-			dialog.addEventListener('click', e => {
-				if (e.target.classList.contains('dialog__close')) {
-					e.preventDefault();
-					dialog.sleekDialog.close();
-				}
-			});
+			}
+			// The target is a dialog element
+			else {
+				this.config.target.sleekDialog.open();
+			}
 		});
 	}
+}
 
-	mountTriggers () {
-		this.config.triggers.forEach(el => {
-			el.addEventListener('click', e => {
+export default class Dialog {
+	constructor (el, conf) {
+		this.el = el;
+		this.config = Object.assign({}, conf);
+	}
+
+	mount () {
+		// Insert methods
+		this.el.sleekDialog = {
+			open: () => {
+				document.documentElement.classList.add('dialog-open');
+				document.documentElement.classList.add('dialog-open--' + this.el.id);
+				this.el.classList.add('open');
+			},
+			close: () => {
+				document.documentElement.classList.remove('dialog-open');
+				document.documentElement.classList.remove('dialog-open--' + this.el.id);
+				this.el.classList.remove('open');
+			},
+			isOpen: () => {
+				return this.el.classList.contains('open');
+			},
+			isClosed: () => {
+				return !this.el.classList.contains('open');
+			},
+			getStatus: () => {
+				return this.el.classList.contains('open') ? 'open' : 'close';
+			}
+		};
+
+		// Insert backdrop
+		var backdrop = document.createElement('div');
+
+		backdrop.classList.add('backdrop');
+		this.el.parentNode.insertBefore(backdrop, this.el.nextSibling);
+
+		backdrop.addEventListener('click', e => {
+			if (e.target === backdrop) {
+				this.el.sleekDialog.close();
+			}
+		});
+
+		// Insert close button
+		var close = document.createElement('a');
+
+		close.classList.add('dialog__close');
+		close.innerHTML = '&times;';
+		this.el.appendChild(close);
+
+		this.el.addEventListener('click', e => {
+			if (e.target.classList.contains('dialog__close')) {
 				e.preventDefault();
-
-				var target = document.getElementById(el.getAttribute('href').substr(1));
-
-				if (target) {
-					// The target is a template
-					if (target.nodeName.toLowerCase() === 'script') {
-						this.templateDialog.className = 'dialog dialog--no-transition ' + target.className;
-						this.templateDialog.innerHTML = target.innerHTML + '<a class="dialog__close">&times;</a>';
-
-						// HACK: Wait for dialog--no-transition to kick in (for some reason I need around 50ms...)
-						setTimeout(() => {
-							this.templateDialog.classList.remove('dialog--no-transition');
-							this.templateDialog.sleekDialog.open();
-						}, 50);
-					}
-					// The target is a dialog element
-					else {
-						target.sleekDialog.open();
-					}
-				}
-			});
+				this.el.sleekDialog.close();
+			}
 		});
 	}
 }
