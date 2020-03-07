e954cb438c1e8ba58b02312fa55639046a7a92e7
powerbuoy <powerbuoy@gmail.com>
2020-03-07 19:07:23 +0100

    Hmm

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,21 @@
-// Utils
 const path = require('path');
 
-// Base config
-var config = {
-	// In
+module.exports = {
 	entry: {
 		'sleek-ui': './src/js/sleek-ui.js'
 	},
-
-	// n out
 	output: {
 		filename: '[name].js',
 		path: path.resolve(__dirname, 'dist'),
 		library: 'sleek-ui',
 		libraryTarget: 'umd'
 	},
-
-	// Config
 	module: {
 		rules: [
-			// JS
 			{
 				test: /\.js$/,
 				exclude: /node_modules/,
 				use: [
-					// Babel
 					{
 						loader: 'babel-loader',
 						options: {
@@ -35,22 +26,4 @@ var config = {
 			}
 		]
 	}
-};
-
-// Finish config
-module.exports = (env, argv) => {
-	// Dev only
-	if (argv.mode === 'development') {
-		// Watch
-		config.watch = true;
-		config.watchOptions = {
-			ignored: /node_modules/,
-			aggregateTimeout: 300
-		};
-
-		// Sourcemaps
-		config.devtool = 'source-map';
-	}
-
-	return config;
 };
