f82afdc4a70489ed65b85814431f97bd728fed20
powerbuoy <powerbuoy@gmail.com>
2020-10-29 15:05:04 +0100

    Allow throttling submitonchange

--- src/js/submit-onchange.js
+++ src/js/submit-onchange.js
@@ -1,17 +1,36 @@
 'use strict';
 
 export default class SubmitOnchange {
-	constructor (form) {
+	constructor (form, conf) {
 		this.form = form;
+		this.timeout = null;
+		this.config = Object.assign({
+			throttle: 0
+		}, conf);
 	}
 
 	mount () {
 		this.form.addEventListener('change', e => {
-			// Trigger onsubmit
-			this.form.dispatchEvent(new Event('submit', {bubbles: true}));
+			if (this.config.throttle) {
+				if (this.timeout) {
+					clearTimeout(this.timeout);
+				}
 
-			// Actually submit the form
-			this.form.submit();
+				this.timeout = setTimeout(() => {
+					this.submit();
+				}, this.config.throttle);
+			}
+			else {
+				this.submit();
+			}
 		});
+	}
+
+	submit () {
+		// Trigger onsubmit
+		this.form.dispatchEvent(new Event('submit', {bubbles: true}));
+
+		// Actually submit the form
+		this.form.submit();
 	}
 }
